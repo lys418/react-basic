9492ea5f7c1aed0cca1bfd30b025d66632edbb3c
melt <lys418@gmail.com>
2019-05-20 12:43:37 +0900

    commit 20190520

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <div>리액트를 배웁시다!!오ㅇㄹㄴㅇㅇㄴㅇ렁ㄴㄹ나</div>
+        <div>react!!!</div>
         <a
           className="App-link"
           href="https://reactjs.org"
