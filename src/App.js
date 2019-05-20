75d972108eb2d5153b92285df954683773746b5d
melt <lys418@gmail.com>
2019-05-20 15:35:12 +0900

    react-study

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'melt';
+  const num = 3;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>react!!!</div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+    <fragments>
+      <div>
+        hello {name}!!!! 
+        <input type="text"/>
+
+        {
+          1+2 === 13 ? (<div>맞다!!</div>) : (<div>틀리다!!</div>)
+        } 
+        </div>
+
+        <div>
+          {
+            (()=> {
+              if(num === 1) return (<div>원</div>);
+              if(num === 2) return (<div>투</div>);
+              if(num === 3) return (<div>쓰리</div>);
+            })()
         
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          }
+
+        </div>
+
+        <div className="App-test">리액트</div>
+      </fragments>
+    );
 }
 
 export default App;
