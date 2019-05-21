af03d80e1a7cf6abfd325661c1e17a23e5eecabe
melt <lys418@gmail.com>
2019-05-22 08:17:54 +0900

    react basic commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import MyName from './MyName';
+import Counter from './Counter'
 
-function App() {
-  const name = 'melt';
-  const num = 3;
-  return (
-    <fragments>
-      <div>
-        hello {name}!!!! 
-        <input type="text"/>
+class App extends Component {
 
-        {
-          1+2 === 13 ? (<div>맞다!!</div>) : (<div>틀리다!!</div>)
-        } 
-        </div>
-
-        <div>
-          {
-            (()=> {
-              if(num === 1) return (<div>원</div>);
-              if(num === 2) return (<div>투</div>);
-              if(num === 3) return (<div>쓰리</div>);
-            })()
-        
-          }
-
-        </div>
-
-        <div className="App-test">리액트</div>
-      </fragments>
+  render() {
+    return (
+      // <MyName name="리액트" age="15"/>
+      <Counter />
     );
+  }
 }
 
 export default App;
+
--- src/MyName.js
+++ src/MyName.js
@@ -1,1 +1,15 @@
-.
+import React from 'react';
+
+
+const MyName = ({name, age}) => {
+       
+    return (
+        <div>
+            안녕하세요! 저는 {name} 입니다!~!!! <br/>
+            나이는 {age}  입니다.
+
+        </div>
+    );
+};
+
+export default MyName;
